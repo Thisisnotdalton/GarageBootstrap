bb19f31020021d2efb9f5f01b7471f124b2d4908
Dalton <dalton@pukiru.games>
2026-05-14 16:55:51 -0400

    Apply configuration cli command.

--- garage_bootstrap/src/garage_bootstrap/main.py
+++ garage_bootstrap/src/garage_bootstrap/main.py
@@ -1,12 +1,15 @@
 import typer
-from garage_bootstrap.garage import
+from garage_bootstrap.garage import apply_configuration
+from garage_bootstrap.models import GarageConfiguration
 
 app = typer.Typer()
 
 
 @app.command()
-def generate_keys(configuration_file_path: str):
-    print(f"Generating API keys for {configuration_file_path}")
+def apply(configuration_file_path: str):
+    print(f"Applying configuration from {configuration_file_path}")
+    with open(configuration_file_path, 'r') as f:
+        apply_configuration(GarageConfiguration.model_validate_json(f.read()))
 
 
 if __name__ == "__main__":
